dd883f4b2aaaf4d16f37df53a3e901fba03f7780
cris <cris.sherban.t@gmail.com>
2021-03-20 10:06:20 +0100

    Snapshots added

--- court_analysis.py
+++ court_analysis.py
@@ -220,6 +220,34 @@ def color_polygon(img, color=0):
     return img
 
 
+def homography(rect, image):
+    bl, tl, tr, br = rect
+    rect = np.array([tl, tr, br, bl], dtype="float32")
+
+
+    # print(rect)
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB)) + 700
+
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+
+    # print(dst)
+
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+    plt_plot(warped)
+    return warped
+
 
 #####################################################################
 if __name__ == '__main__':
