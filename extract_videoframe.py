7ecc080fa17040538c8affc0af68c7aab61c78db
frenk <francesco.vannoni@stud.unifi.it>
2021-03-29 16:27:17 +0200

    extract videoframe to cut ball

---
+++ extract_videoframe.py
@@ -0,0 +1,42 @@
+import cv2
+import os
+
+def extract_videoframe(video_dir):
+    # Read the video from specified path
+    cam = cv2.VideoCapture("resources/Short4Mosaicing.mp4")
+
+    try:
+
+        # creating a folder named data
+        if not os.path.exists('data'):
+            os.makedirs('data')
+
+    # if not created then raise error
+    except OSError:
+        print('Error: Creating directory of data')
+
+    # frame
+    currentframe = 0
+
+    while (True):
+
+        # reading from frame
+        ret, frame = cam.read()
+
+        if ret:
+            # if video is still left continue creating images
+            name = './data/frame' + str(currentframe) + '.jpg'
+            print('Creating...' + name)
+
+            # writing the extracted images
+            cv2.imwrite(name, frame)
+
+            # increasing counter so that it will
+            # show how many frames are created
+            currentframe += 1
+        else:
+            break
+
+    # Release all space and windows once done
+    cam.release()
+    cv2.destroyAllWindows()
